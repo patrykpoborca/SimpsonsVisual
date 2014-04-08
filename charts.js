eb8aeb619c9307cce4238651c0202523630548aa
wericcirew <icstest315@gmail.com>
2014-04-07 15:41:49 -1000

    Code for generating charts from input
    Add some comments and notes on what is missing for it to work with the
    UI.

--- charts.js
+++ charts.js
@@ -27,6 +27,19 @@ function drawChart() {
 	google.visualization.events.addListener(chart, 'click', handler);		
 }*/
 
+/*
+============================================================================================
+
+For the lines of code that you see y: some integer value, I was thinking that we need a method or something to count the
+amount of appearances through that particular season.  In some of the entries, I wrote a generic method call getEpi that
+returns a number for testing and it works fine.
+
+Advice: Whoever wants to or if I do it after figuring out a way to access that data that I need.  The function to get the episode
+count may need to take in the character name and the season for faster searching.
+
+============================================================================================
+*/
+
 var charaArray = new Array();
 var seasonArray = new Array();
 
@@ -34,10 +47,23 @@ $().ready(function () {
 	var chart = new CanvasJS.Chart("chart_div", {
 		theme: "theme3",
 		title: {
-			text: "The Simpsons"
+			text: "The Simpsons",
+			fontSize: 30
+		},
+		axisX: {
+			title: "Seasons",
+			margin: 0
+		},
+		axisY: {
+			title: "Appearances of Characters",
+			titleFontSize: 20
 		},
 		legend: {
-			fontFamily: "sans-serif"
+			fontFamily: "sans-serif",
+			verticalAlign: "bottom",
+			horizontalAlign: "center",
+			cursor: "pointer",
+			//itemclick: function
 		},
 		creditText: "",
 		toolTip: {
@@ -90,7 +116,7 @@ function generateData() {
 			dataPoints: points,
 		}
 		if(charaArray.length == 1) {
-			values["color"] = "#9ACD32";
+			values["color"] = "#FFD90F";
 		}
 		data.push(values);
 	}
@@ -98,12 +124,29 @@ function generateData() {
 	return data;
 }
 
-function createChart() {
+function createSeasonChart() {
 	//console.log(generateData());
 	var chart = new CanvasJS.Chart("chart_div", {
 		theme: "theme3",
+		title: {
+			text: "Appearance by Season",
+			fontSize: 30
+		},
+		axisX: {
+			title: "Seasons",
+			margin: 0
+		},
+		axisY: {
+			title: "# of Appearances",
+			titleFontSize: 20
+		},
 		legend: {
-			fontFamily: "sans-serif"
+			fontFamily: "sans-serif",
+			verticalAlign: "bottom",
+			horizontalAlign: "center",
+			cursor: "pointer",
+			// A function call to switch to a more focused chart.  Which will show all the episode in the seasons and show if the characters showed up in the episode
+			//itemclick: function
 		},
 		creditText: "",
 		toolTip: {
@@ -123,7 +166,7 @@ function getValue(sel) {
 		charaArray = $(sel).val();
 	}
 	if(seasonArray.length >= 1 && charaArray.length >= 1) {
-		createChart();
+		createSeasonChart();
 	}
 	//console.log(theValue);
 	//createChart(theValue);
