e7f816717e0de848c150e8b082d4c62094d1af94
Patryk Jacek Poborca <poborcapatryk@gmail.com>
2014-04-15 08:37:05 -0500

    Merged all our stuff as stated earlier, sliders are dynamic, there are 4 toggles, default is dylan's chart, can toggle through the debuglog/toggle button, my table can be toggled between graphical and textual by clicking on it. Will add more stuff later...

--- htmlPAGES/app/assets/js/patrykWork/communicateUI.js
+++ htmlPAGES/app/assets/js/patrykWork/communicateUI.js
@@ -23,14 +23,39 @@ function debugLogCom()
 	}
 }
 
-
+var animationQ = [];
 
 // custom function methods for initiation
 function initUI()
 {
+
+	//animation stuff
+	animationQ[0] = { name : "superDiv", status : true};//showing by default.
+	animationQ.push({name : "chart_div", status : false}); //hidden
+	animationQ.push({name : "genTable", status : false}); //hidden
+	$("#genTable").toggle(function(){
+	$(this).empty();
+	var HOLD = wrapForTable(p_charBySeason());
+	initTable(HOLD.length, HOLD[0].length, "genTable", HOLD, true, false); // rows, columns, divID, data, display 
+	HOLD = [];
+	}
+	,
+	function()
+	{
+	$(this).empty();
+	var HOLD = wrapForTable(p_charBySeason());
+	initTable(HOLD.length, HOLD[0].length, "genTable", HOLD, false, false); // rows, columns, divID, data, display 
+	HOLD = [];
+	}	
+	);
+	
 	//initialization...
+	$('#superDiv').on('change', chartFunc());
 	fillArrayAllCharByAppearAmt();
 	fillAllEpsArray();
+	chartFunc();
+	$("#chart_div").hide(); //for king div
+	$("#genTable").hide(); //for king div
 	
 	// hiding stuff... moving stuff around...
 	$("#expandSelectors").hide();	
@@ -38,14 +63,19 @@ function initUI()
 	$("#toggler").toggle(  // to toggle the nav bar
 	 function()
 	 {
-	 //$("#sideBar").slideUp(500);
 	  $("#sideBar").animate({width : "0px", height : "0px"},1180, function(){$("#sideBar").hide();});
 	  $("#sideBar *").each(function(){$(this).hide();});
-	  
-	 $("#chart_div").animate({left:"50px"}, 1000);
 	 $("#expandSelectors").slideUp();
 	 $("#expandSliders").text("Show Sliders");
 	 $(this).animate({width : "50px"}, 1000);
+	 //end of other stuff
+	 for(var z =0 ; z < animationQ.length; z++)
+	 	{var x = animationQ[z];
+		if(x["status"])
+			$("#" + x['name']).animate({left:"50px"}, 1000);
+			}
+	
+	 
 	 }
 	 ,
 	 function()
@@ -60,9 +90,14 @@ function initUI()
 		 });
 	  
 	 
-	 $(this).animate({width : "250px"});
-	  $("#chart_div").animate({left:"255px"});
+	 $(this).animate({width : "255px"});
 	  $("#sideBar").slideDown();
+	   for(var z =0 ; z < animationQ.length; z++)
+	 	{var x = animationQ[z];
+		if(x["status"])
+			$("#" + x['name']).animate({left:"255px"}, 1000);
+			}
+	  
 	 }
 	 ); // end of nav bar toggle
 
@@ -79,4 +114,33 @@ function initUI()
 		$("#expandSelectors").slideUp();	
 	}
 	);
+	
+	$("#sendData").toggle(
+	function()
+	{
+	$("#superDiv").hide();
+	$("#chart_div").show();
+	console.log($("#chart_div").css('width'));
+	$("#genTable").show();
+	flipFlagsUi();
+	}
+	,
+	function()
+	{
+	$("#superDiv").show();
+	$("#chart_div").hide();
+	$("#genTable").hide();
+	flipFlagsUi();
+	});
+	
+	
 }
+
+
+function flipFlagsUi()
+{
+for(var z =0 ; z < animationQ.length; z++)
+	animationQ[z]["status"] = !animationQ[z]["status"];		
+}
+		
+
---
+++ htmlPAGES/app/assets/js/patrykWork/parseData.js
@@ -0,0 +1,108 @@
+
+function p_charBySeason()
+{
+var printFunc = function()
+{
+var debugString = "";
+for(var it = 0; it < finalArray.length; it++)
+	{
+		debugString = "Rows = ";
+		
+		for(var it2= 0; it2< finalArray[it].length; it2++)
+			{debugString+= finalArray[it][it2]+ " ";
+			
+			if(finalArray.length == 1)
+			console.log("kkk " + finalArray[it][it2]); }
+			
+			if(finalArray.length != 1)console.log(debugString);
+	}
+}
+//////////////////////////////
+
+var episodesRow = [];
+episodesRow[0] = "";
+for(var ii = 1; ii < dkGlobalOverviewTable.arrayOfEpisodesInSeasons.length +1; ii++)
+	{episodesRow.push(ii);}
+	
+
+var listOfChars = p_fetchChars(0);
+
+var finalArray = [];
+
+finalArray.push(episodesRow);
+
+		
+	
+
+
+
+var temp = [];
+var holder ;
+for(var firstTier = 0; firstTier < listOfChars.length; firstTier++) //start at 1 to avoid dead spot 0/0
+{
+	temp[0] = listOfChars[firstTier][0]; //save name
+	holder = p_iterateCharEpisodes(listOfChars[firstTier]);
+	for(var secondTier = 0; secondTier < holder.length; secondTier++)
+	temp.push(holder[secondTier]);
+	finalArray.push(temp);
+	temp = [];
+}
+
+
+
+return finalArray;
+}
+var p_count =0;
+//
+function p_iterateCharEpisodes(character) //condition = function e.g. cond = function(){return true;}; .. todo?
+{
+	p_count ++;
+	var r_val = [];
+	
+	for(var zz = 0; zz < dkGlobalOverviewTable.arrayOfEpisodesInSeasons.length; zz++) 
+	{r_val[zz] = 0; }
+	var gugle = 0;
+	
+	for(var a = 0; a < character[7].length; a++)
+	{	
+		if(getSeasonOfEpisodeNumber(getEpsNumber(character[7][a])) != null)
+		{
+		r_val[Number(getSeasonOfEpisodeNumber(getEpsNumber(character[7][a])))] ++;
+		}
+	}
+	var debug_string = character[0] + " ";
+	for(var x = 0; x < r_val.length; x ++)
+		debug_string += r_val[x];
+	//console.log(debug_string);
+	return r_val;
+}
+
+
+
+
+function p_fetchChars(amount)
+{
+if(amount == 0) amount = allCharByAppearAmt.length;
+var charList = [];
+for(var x = 0; x < amount; x ++)
+	charList.push(allCharByAppearAmt[x]);
+
+	return charList;		
+	}
+
+
+	
+/////////////////////////// debug functions... save
+function console2D(arr)
+{
+	var debugstring = "";
+	for(var first = 0; first < arr.length; first ++)
+	{
+	debugstring = "";
+	for(var second = 0; second < arr[first].length; second++)
+	{
+	debugstring+= arr[first][second] + " ";
+		}
+	console.log(debugstring);
+	}
+}
